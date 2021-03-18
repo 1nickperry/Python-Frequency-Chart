02d705dad59dc2a468b79cdb9861ee766c5947e6
nunya bidness <1nickperry@gmail.com>
2021-03-17 22:53:00 -0600

    initial commit

---
+++ add.py
@@ -0,0 +1,7 @@
+notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+
+for x in range(0,10):
+    for i in range(0,10):
+        print("%s%s" % (notes[i], x))
+#  print(notes + x, end=' ')
+
